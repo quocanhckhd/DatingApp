4bc79f00469d4ca1f757a11ec7a8aff6bde388df
quocanhckhd@gmail <quocanhckhd@gmail.com>
2019-12-22 07:43:17 +0700

    End 6

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,83 +1,78 @@
-using System.Text;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 using DatingApp.API.Models.DTOs;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
 
-namespace DatingApp.API.Controllers
-{
-    [Route("api/[controller]")]
+namespace DatingApp.API.Controllers {
+    [Route ("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
-    {
+    public class AuthController : ControllerBase {
         private readonly IAuthRepository _repo;
         private readonly IConfiguration _config;
-        public AuthController(IAuthRepository repo, IConfiguration config)
-        {
+        public AuthController (IAuthRepository repo, IConfiguration config) {
             _repo = repo;
             _config = config;
         }
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register(RegisterDTO dto)
-        {
+        [HttpPost ("register")]
+        public async Task<IActionResult> Register (RegisterDTO dto) {
             //Validate Request
-            if(!ModelState.IsValid)
-                return BadRequest(ModelState);
+            if (!ModelState.IsValid)
+                return BadRequest (ModelState);
 
-            dto.UserName = dto.UserName.ToLower();
-            if(await _repo.UserExisted(dto.UserName)) return BadRequest("User already exists");
+            dto.UserName = dto.UserName.ToLower ();
+            if (await _repo.UserExisted (dto.UserName)) return BadRequest ("User already exists");
 
-            var userToCreate = new User 
-            {
+            var userToCreate = new User {
                 UserName = dto.UserName
             };
 
-            var createdUser = await _repo.Register(userToCreate, dto.Password);
+            var createdUser = await _repo.Register (userToCreate, dto.Password);
 
-            return StatusCode(201);
+            return StatusCode (201);
         }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login(LoginDTO dto)
-        {
-            //Validate Request 
-            var user = await _repo.Login(dto.UserName, dto.Password);
+        [HttpPost ("login")]
+        public async Task<IActionResult> Login (LoginDTO dto) {
 
-            if(user == null) return Unauthorized();
+            try {
+                var user = await _repo.Login (dto.UserName, dto.Password);
 
-            var claims = new []
-            {
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),                
-                new Claim(ClaimTypes.Name, user.UserName)
-            };
+                if (user == null) return Unauthorized ();
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8
-                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+                var claims = new [] {
+                    new Claim (ClaimTypes.NameIdentifier, user.Id.ToString ()),
+                    new Claim (ClaimTypes.Name, user.UserName)
+                };
 
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+                var key = new SymmetricSecurityKey (Encoding.UTF8
+                    .GetBytes (_config.GetSection ("AppSettings:Token").Value));
 
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(1),
-                SigningCredentials = creds
-            };
+                var creds = new SigningCredentials (key, SecurityAlgorithms.HmacSha512Signature);
 
-            var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenDescriptor = new SecurityTokenDescriptor {
+                    Subject = new ClaimsIdentity (claims),
+                    Expires = DateTime.Now.AddDays (1),
+                    SigningCredentials = creds
+                };
 
-            var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenHandler = new JwtSecurityTokenHandler ();
 
-            return Ok(
-            new 
-            {
-                token = tokenHandler.WriteToken(token)
-            });
+                var token = tokenHandler.CreateToken (tokenDescriptor);
+
+                return Ok (
+                    new {
+                        token = tokenHandler.WriteToken (token)
+                    });
+            } catch {
+                return StatusCode (500, "Computers really says no");
+            }
         }
     }
 }
